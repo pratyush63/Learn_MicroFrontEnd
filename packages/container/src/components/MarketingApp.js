9c710ad8cda6a7fa8646fc3d624231b0bae466d3
unknown <pratyush63.kumar@gmail.com>
2021-03-23 18:49:25 +0530

    Multi-Tier Navigation added

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,26 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
-
+    const history = useHistory();
+    
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+        //the listen function which called the onNavigate function returns an object argument which has the path name, path key etc as properties
+            onNavigate: ({ pathname: nextPathname }) =>{
+                //console.log('The container noticed navigation in marketing (means memoryHistory of marketing is updated), so update BrowserHistory of container');
+                const { pathname } = history.location;//Address of the current location
+
+                if(pathname !== nextPathname){
+                history.push(nextPathname);
+                }
+            }
+        });
+        //Anytime when there is a change in browserhistory , call onParentNavigate function
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 //Mount function to start up the app
-const mount= (el) => {
+const mount= (el, { onNavigate, deafultHistory }) => {
+    
+    //IF browserHistory is created then use it else create a memory history 
+    const history = defaultHistory || createMemoryHistory();
+    //Whenever some navigation occurs this history object is going to call any function that we have provided
+    if(onNavigate){
+    history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={ history } />,
         el
     );
+
+    return {
+        onParentNavigate({ pathname: nextPathname}){
+            //console.log('Container just navigated');
+            const { pathname } = history.location;
+            if(pathname !== nextPathname){
+                history.push(nextPathname);
+            }
+        },
+    };
 };
 
 //If we are in development or in isolation call mount immediately 
 if(process.env.NODE_ENV === 'development'){
     const devRoot = document.querySelector('#_marketing-dev-root');
-
+    
+    //Create Browser history if app runs in isolation in development
     if(devRoot){
-        mount(devRoot);
+        mount(devRoot, { deafultHistory: createBrowserHistory() });
     }
 }
 
