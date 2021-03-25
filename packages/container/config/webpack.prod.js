70c3ac07fdab527a531d3d6bd7abcd3c97d19414
unknown <pratyush63.kumar@gmail.com>
2021-03-26 00:58:21 +0530

    temporary

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,19 +8,18 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-      filename: '[name].[contenthash].js',
-      //For AWS S3 bucket as main.js file is in container latest
-      publicPath: '/container/latest/', 
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
-      new ModuleFederationPlugin({
-         name: 'container',
-         remotes: {
-           marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-           auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-         },
-         shared: packageJson.dependencies,
-      }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
   ],
 };
 
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,23 +1,23 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
   output: {
-      filename: '[name].[contenthash].js',
-      publicPath: '/marketing/latest/',
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
   },
   plugins: [
-      new ModuleFederationPlugin({
-         name: 'marketing',
-         filename: 'remoteEntry.js',
-         exposes: {
-            './MarketingApp': './src/bootstrap'
-        },
-         shared: packageJson.dependencies,
-      }),
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
   ],
 };
 
